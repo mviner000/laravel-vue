4cd3f5b5c97b5f2c5316c445f5cd0d3fe869e16c
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-21 15:34:49 +0800

    public build added for deploy

---
+++ public/build/assets/DeleteUserForm-B8VgUiln.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-DI1QP3ZF.js";import"./app-CfUEMLMu.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-ah8j87XW.js";export{o as default};
---
+++ public/build/assets/UpdatePasswordForm-C-wRotp2.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdatePasswordForm.vue_vue_type_script_setup_true_lang-Cg0vXU-r.js";import"./app-CfUEMLMu.js";import"./TextInput.vue_vue_type_script_setup_true_lang-ah8j87XW.js";import"./PrimaryButton-BEI0njYy.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
