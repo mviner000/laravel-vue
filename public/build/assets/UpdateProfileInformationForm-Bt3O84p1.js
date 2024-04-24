026a9fc8f05cb1fd9492fbc8a518d41c079d1beb
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-24 22:33:00 +0800

    build added for testing viewtable

---
+++ public/build/assets/UpdateProfileInformationForm-Bt3O84p1.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdateProfileInformationForm.vue_vue_type_script_setup_true_lang-BrI94YaO.js";import"./app-Bl4LwM3v.js";import"./TextInput.vue_vue_type_script_setup_true_lang-BY8CAFVd.js";import"./PrimaryButton-C__J9fZy.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
